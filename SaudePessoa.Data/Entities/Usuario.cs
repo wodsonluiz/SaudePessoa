d3cddeed15a8cdacf87164dfaa39bdbf9914693a
wodson.correia <wodson.correia@BSP-4145>
2019-07-18 17:08:13 -0300

    commit initial JWT

--- SaudePessoa.Data/Entities/Usuario.cs
+++ SaudePessoa.Data/Entities/Usuario.cs
@@ -2,8 +2,10 @@ namespace SaudePessoa.Data.Entities
 {
     public class Usuario
     {
+        public int Id { get; set; }
         public int Id_Pessoa { get; set; }
         public string Email { get; set; }
         public string Senha { get; set; }
+        public int Status { get; set; }
     }
 }
---
+++ SaudePessoa.Data/Service/RepositoryUsuario.cs
@@ -0,0 +1,24 @@
+﻿using SaudePessoa.Data.Entities;
+using SaudePessoa.Data.Interface;
+using System;
+
+namespace SaudePessoa.Data.Service
+{
+    public class RepositoryUsuario : IRepositoryUsuario
+    {
+        public bool Desativar(string email, string _connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Insert(Usuario pessoa, string _connection)
+        {
+            throw new NotImplementedException();
+        }
+
+        public bool Logar(string email, string password, string _connection)
+        {
+            throw new NotImplementedException();
+        }
+    }
+}
